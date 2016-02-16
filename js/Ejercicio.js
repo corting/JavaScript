290ff522855bb49fcf5cbe18871c131239b13dfb
corting <f.ponce.serrano@gmail.com>
2016-02-16 18:20:32 +0100

    java ejercicio1

---
+++ js/Ejercicio.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var num1 = 1;
+var num2 = 2;
+
+var sum = num1+num2;
+
+console.log(sum);
+
+
+// alert(a); // Esto muestra una ventana emergente
+
+// Almacenar en una variable el resultado de dividir 6 entre 2
+
+var div;
+var num3 = 6;
+div = num3/num2;
+
+console.log(div);
+
+// Almacenar en una variable el precio de un artículo de 20€ aplicándole el 21% de IVA .
+var iva = 0.21;
+var iva_pri = 20 * iva;
+var price =  20 + iva_pri;
+console.log(price);
+// Definir una variable con el valor 4 y utilizar el operador (```++```) para incrementar en uno su valor.
+var num4 = 4;
+num4++;
+
+console.log(num4);
+
+// Definir una variable que almacene la concatenación de dos cadenas de texto.
+var txt1 = 'hola';
+var txt2 = 'Que tal?';
+
+var txt = txt1 + txt2;
+console.log(txt);
+
+// Definir una variable ```price``` con el valor ```19.99```y aplicar la abreviación ```/=``` para dividirlo entre ```1.21``` para obtener el precio sin IVA.
+
+var price = 19.99;
+
+var abre = price /= 1.21;
+console.log(abre);
+
+// Asignar a dos variables valores booleanos y hacer al menos una operación combinando un operador lógico: **AND** (```&&```) o **OR** (```||```)
+
+var verdader = true;
+var falso = false;
+
+var op = verdader && falso
+
+console.log(op);
+
+// Realizar 4 expresiones que utilicen operadores relacionales (```<```, ```==```, ```!=``` y ```===```) y almacenen los valores en tres variables distintas.
+
+var exp1 = iva < num1;
+
+console.log(exp1);
+
+
